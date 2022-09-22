0b28b76fc196d2f5bba9f10d57ce35192f55a601
hayabusabusa <hayabusabusaSY@gmail.com>
2022-09-23 00:41:32 +0900

    feat: メトロノーム画面の View Model を追加

---
+++ Package/Sources/MetronomeFeature/MetronomeViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  MetronomeViewModel.swift
+//  
+//
+//  Created by Shunya Yamada on 2022/09/22.
+//
+
+import Audio
+import Combine
+import Foundation
+
+public final class MetronomeViewModel {
+
+    private let hapticService: HapticService?
+
+    @Published var error: Error?
+    @Published var isPlaying = false
+
+    public init(hapticService: HapticService?) {
+        self.hapticService = hapticService
+
+        // NOTE: CoreHaptic 関連クラスの初期化に失敗した場合はエラーを流す.
+        if hapticService == nil {
+            error = NSError(domain: "CoreHapticError", code: -1, userInfo: nil)
+        }
+    }
+
+    func onPlayButtonPressed() {
+        guard let hapticService = hapticService else {
+            return
+        }
+
+        do {
+            if isPlaying {
+                hapticService.stop()
+            } else {
+                try hapticService.play()
+            }
+        } catch {
+            self.error = error
+        }
+
+        isPlaying = hapticService.isPlaying
+    }
+}
