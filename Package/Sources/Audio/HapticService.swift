058d4a2fb78315cfea76822752277452de77df6c
hayabusabusa <hayabusabusaSY@gmail.com>
2022-09-23 13:19:08 +0900

    feat: Haptic 関連クラスに BPM を変更する機能を実装

--- Package/Sources/Audio/HapticService.swift
+++ Package/Sources/Audio/HapticService.swift
@@ -31,6 +31,7 @@ public final class HapticService {
                 print("Stop Handler: The engine stopped for reason: \(reason.rawValue)")
             }
             try hapticEngine.start()
+            try configurePlayer()
         } catch {
             throw error
         }
@@ -38,34 +39,59 @@ public final class HapticService {
 
     public func play() throws {
         do {
-            if player == nil {
-                guard let url = AudioFileService.resource(for: .click) else { return }
-
-                let audioResourceID = try hapticEngine.registerAudioResource(url)
-                let hapticEvents = [
-                    CHHapticEvent(audioResourceID: audioResourceID,
-                                  parameters: [
-                                    .init(parameterID: .audioVolume, value: 1.0),
-                                  ],
-                                  relativeTime: 0,
-                                  duration: beats.intervalForBPM),
-                ]
-                let pattern = try CHHapticPattern(events: hapticEvents, parameters: [])
-
-                player = try hapticEngine.makeAdvancedPlayer(with: pattern)
-                player?.loopEnabled = true
-            }
-
             try player?.start(atTime: CHHapticTimeImmediate)
-
             isPlaying = true
         } catch {
             throw error
         }
     }
 
-    public func stop() {
-        try? player?.stop(atTime: CHHapticTimeImmediate)
-        isPlaying = false
+    public func stop() throws {
+        do {
+            try player?.stop(atTime: CHHapticTimeImmediate)
+            isPlaying = false
+        } catch {
+            throw error
+        }
+    }
+
+    public func change(bpm: Double) throws {
+        do {
+            // NOTE: BPM を変えて Player を生成し直す.
+            // Player の生成し直しはそこまでコストが高くない.
+            // https://developer.apple.com/documentation/corehaptics/updating_continuous_and_transient_haptic_parameters_in_real_time
+            beats.change(bpm: bpm)
+            try configurePlayer()
+
+            if isPlaying {
+                try play()
+            }
+        } catch {
+            throw error
+        }
+    }
+}
+
+private extension HapticService {
+    func configurePlayer() throws {
+        do {
+            guard let url = AudioFileService.resource(for: .click) else { return }
+
+            let audioResourceID = try hapticEngine.registerAudioResource(url)
+            let hapticEvents = [
+                CHHapticEvent(audioResourceID: audioResourceID,
+                              parameters: [
+                                .init(parameterID: .audioVolume, value: 1.0),
+                              ],
+                              relativeTime: 0,
+                              duration: beats.intervalForBPM),
+            ]
+            let pattern = try CHHapticPattern(events: hapticEvents, parameters: [])
+
+            player = try hapticEngine.makeAdvancedPlayer(with: pattern)
+            player?.loopEnabled = true
+        } catch {
+            throw error
+        }
     }
 }
