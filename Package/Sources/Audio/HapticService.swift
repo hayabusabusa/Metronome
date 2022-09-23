748dc8366f155f45b9a1b2f5b592e6ddce4b421b
hayabusabusa <hayabusabusaSY@gmail.com>
2022-09-23 18:10:12 +0900

    fix: Haptic 関連のクラスを修正
    内部で AVAudioSession を管理するクラスを利用するように修正。

--- Package/Sources/Audio/HapticService.swift
+++ Package/Sources/Audio/HapticService.swift
@@ -10,7 +10,8 @@ import CoreHaptics
 import Foundation
 
 public final class HapticService {
-    private let audioSession: AVAudioSession
+    private let audioSessionService = AudioSessionService.shared
+
     private let hapticEngine: CHHapticEngine
     private var player: CHHapticAdvancedPatternPlayer?
 
@@ -18,80 +19,58 @@ public final class HapticService {
     private(set) public var isPlaying = false
 
     public init() throws {
-        do {
-            audioSession = AVAudioSession.sharedInstance()
-            try audioSession.setCategory(.playback)
-            try audioSession.setActive(true)
+        try audioSessionService.activate()
 
-            hapticEngine = try CHHapticEngine(audioSession: audioSession)
-            hapticEngine.resetHandler = { [weak self] in
-                try? self?.hapticEngine.start()
-            }
-            hapticEngine.stoppedHandler = { reason in
-                print("Stop Handler: The engine stopped for reason: \(reason.rawValue)")
-            }
-            try hapticEngine.start()
-            try configurePlayer()
-        } catch {
-            throw error
+        hapticEngine = try CHHapticEngine(audioSession: audioSessionService.session)
+        hapticEngine.resetHandler = { [weak self] in
+            try? self?.hapticEngine.start()
         }
+        hapticEngine.stoppedHandler = { reason in
+            print("Stop Handler: The engine stopped for reason: \(reason.rawValue)")
+        }
+        try hapticEngine.start()
+        try configurePlayer()
     }
 
     public func play() throws {
-        do {
-            try player?.start(atTime: CHHapticTimeImmediate)
-            isPlaying = true
-        } catch {
-            throw error
-        }
+        try player?.start(atTime: CHHapticTimeImmediate)
+        isPlaying = true
     }
 
     public func stop() throws {
-        do {
-            try player?.stop(atTime: CHHapticTimeImmediate)
-            isPlaying = false
-        } catch {
-            throw error
-        }
+        try player?.stop(atTime: CHHapticTimeImmediate)
+        isPlaying = false
     }
 
     public func change(bpm: Double) throws {
-        do {
-            // NOTE: BPM を変えて Player を生成し直す.
-            // Player の生成し直しはそこまでコストが高くない.
-            // https://developer.apple.com/documentation/corehaptics/updating_continuous_and_transient_haptic_parameters_in_real_time
-            beats.change(bpm: bpm)
-            try configurePlayer()
+        // NOTE: BPM を変えて Player を生成し直す.
+        // Player の生成し直しはそこまでコストが高くない.
+        // https://developer.apple.com/documentation/corehaptics/updating_continuous_and_transient_haptic_parameters_in_real_time
+        beats.change(bpm: bpm)
+        try configurePlayer()
 
-            if isPlaying {
-                try play()
-            }
-        } catch {
-            throw error
+        if isPlaying {
+            try play()
         }
     }
 }
 
 private extension HapticService {
     func configurePlayer() throws {
-        do {
-            guard let url = AudioFileService.resource(for: .click) else { return }
+        guard let url = AudioFileService.resource(for: .click) else { return }
 
-            let audioResourceID = try hapticEngine.registerAudioResource(url)
-            let hapticEvents = [
-                CHHapticEvent(audioResourceID: audioResourceID,
-                              parameters: [
-                                .init(parameterID: .audioVolume, value: 1.0),
-                              ],
-                              relativeTime: 0,
-                              duration: beats.intervalForBPM),
-            ]
-            let pattern = try CHHapticPattern(events: hapticEvents, parameters: [])
+        let audioResourceID = try hapticEngine.registerAudioResource(url)
+        let hapticEvents = [
+            CHHapticEvent(audioResourceID: audioResourceID,
+                          parameters: [
+                            .init(parameterID: .audioVolume, value: 1.0),
+                          ],
+                          relativeTime: 0,
+                          duration: beats.intervalForBPM),
+        ]
+        let pattern = try CHHapticPattern(events: hapticEvents, parameters: [])
 
-            player = try hapticEngine.makeAdvancedPlayer(with: pattern)
-            player?.loopEnabled = true
-        } catch {
-            throw error
-        }
+        player = try hapticEngine.makeAdvancedPlayer(with: pattern)
+        player?.loopEnabled = true
     }
 }
