3a4df2b828059ce6050f32b3b0652860126ff328
hayabusabusa <hayabusabusaSY@gmail.com>
2022-09-24 12:16:14 +0900

    feat: 録音画面の ViewController を追加

---
+++ Package/Sources/RecordingFeature/RecordingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecordingViewController.swift
+//  
+//
+//  Created by Shunya Yamada on 2022/09/24.
+//
+
+import UIKit
+
+public final class RecordingViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
